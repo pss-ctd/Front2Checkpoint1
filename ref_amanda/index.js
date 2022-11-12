fb7641cdf37099224e3f45dc64babe9409b8448b
CaioNunesS <caiogrb@gmail.com>
2022-11-11 21:08:49 -0300

    pegando o value

--- ref_amanda/index.js
+++ ref_amanda/index.js
@@ -1,12 +1,45 @@
 let btnEnviar = document.getElementById("btnEnviar");
 
-// btnEnviar.addEventListener("submit", function() {
-//     preventDefault();
+let titulo = ''
+let imagem = ''
+let descricao = ''
 
-//     let titulo = document.getElementById("titulo").value;
-//     console.log(titulo);
-// });
+btnEnviar.addEventListener("click", function(event) {
+    event.preventDefault()
+    // btnEnviar = formsReset()
 
+     titulo = document.getElementById("titulo").value
+     imagem = document.getElementById("urlimagem").value
+     descricao = document.getElementById("descricao").value
+  
+    // titulo.addEventListener
 
-let titulo = document.getElementById("titulo");
-titulo.value = "receita de bolo de cenoura";
+    // console.log(titulo);
+    // console.log(imagem);
+    // console.log(descricao);
+});
+
+// function checkUrl(string) {
+//     try {
+//      let url = new URL(string)
+//      console.log("Valid URL!")
+//    } catch(err) {
+//        console.log("Invalid URL!")
+//    }
+//  }
+checkUrl(imagem)
+
+let receitas = {
+    img: `${imagem}`,
+    titulo: `${titulo}`,
+    descricao: `${descricao}`
+
+}
+
+ 
+
+// let titulo = document.getElementById("titulo");
+// titulo.value = "receita de bolo de cenoura";
+// titulo = addEventListener('keydown', function(event){
+
+// })
