7ed7edcf2b85a650406aec2e203cdd537f61fc19
CaioNunesS <caiogrb@gmail.com>
2022-11-12 00:40:57 -0300

    Ainda tentando adicionar o card

--- index.js
+++ index.js
@@ -1,23 +1,55 @@
 let btnEnviar = document.getElementById("btnEnviar");
 
-let titulo = ''
-let imagem = ''
-let descricao = ''
+let tituloP = ''
+let imagemP = ''
+let descricaoP = ''
+
+let section = document.getElementById('section')
 
 btnEnviar.addEventListener("click", function(event) {
     event.preventDefault()
     // btnEnviar = formsReset()
 
-     titulo = document.getElementById("titulo").value
-     imagem = document.getElementById("urlimagem").value
-     descricao = document.getElementById("descricao").value
+    let titulo = document.getElementById("titulo").value
+    let imagem = document.getElementById("urlimagem").value
+    let descricao = document.getElementById("descricao").value
   
-    // titulo.addEventListener
+    tituloP = titulo
+    imagemP = imagem
+    descricaoP = descricao
+
+    // let divP = document.getElementById('divP')
+    // if( true){
+    //     divP.appendChild(section)
+    // }
 
     // console.log(titulo);
     // console.log(imagem);
     // console.log(descricao);
+
+    
+let receitas = {
+    img: imagemP,
+    titulo: tituloP,
+    descricao: descricaoP
+
+}
+// console.log(receitas.titulo);
+// console.log(receitas.descricao);
+let divP = document.getElementById('divP')
+
+section.innerHTML = `
+<img src= ${receitas.img}>
+      
+        <h5 class="card-title">${receitas.titulo}</h5>
+        <p class="card-text" >${receitas.descricao}</p>
+        <button type="button" class="btn btn-primary btn-primary-custom" data-bs-toggle="modal" data-bs-target="#Bobodecamaraomoldal">Ver Receita</button>
+      </div>    `
+  
+
+divP.appendChild(section)
 });
+console.log();
 
 // function checkUrl(string) {
 //     try {
@@ -27,14 +59,24 @@ btnEnviar.addEventListener("click", function(event) {
 //        console.log("Invalid URL!")
 //    }
 //  }
-checkUrl(imagem)
+// checkUrl(imagem)
 
-let receitas = {
-    img: `${imagem}`,
-    titulo: `${titulo}`,
-    descricao: `${descricao}`
 
-}
+// card.innerHTML = `
+//       <img src="${receitas.img}">
+//       <h2>${receitas.titulo}</h2>
+//       <p>${receitas.descricao}</p>
+// `
+
+// .appendChild(div)
+// console.log(receitas.titulo);
+
+// let divP = document.getElementById('divP')
+
+
+// console.log(divP);
+
+
 
  
 
